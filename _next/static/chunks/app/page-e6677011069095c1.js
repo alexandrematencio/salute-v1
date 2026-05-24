c737d61847e6093f190d864e45bb4370ce141d96
Alexandre Matencio <amatencio@pm.me>
2026-05-24 14:05:14 +0200

    deploy: clean rewrite

---
+++ _next/static/chunks/app/page-e6677011069095c1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[68,416,571,774,843,974],{4821:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2619,23)),Promise.resolve().then(n.t.bind(n,1356,23))}},e=>{e.O(0,[619,356,441,255,358],()=>e(e.s=4821)),_N_E=e.O()}]);
